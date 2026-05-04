db1480b08bd234c366a1ba6012d7ee36eb87c2ec
Gerson Eustáquio dos Santos <gebhsantos@gmail.com>
2026-05-04 13:42:55 -0300

    Update main.c

--- ex3/main.c
+++ ex3/main.c
@@ -1,12 +1,20 @@
-void imprime(int v[4])
-{
+#include <stdio.h>
+
+void imprime(const int v[4]) {
     int i, j;
-    for (i = 0; i <= 3; i++)
-    {
-        for (j = 0 ; j <= 3; j++)
-        if (i == j)
-        printf("%d ", v[i]);
-        else
-        printf("%d ", 0);
+    for (i = 0; i <= 3; i++) {
+        for (j = 0; j <= 3; j++) {
+            if (i == j)
+                printf("%d ", v[i]);
+            else
+                printf("%d ", 0);
+        }
+        printf("\n"); /* quebra a linha para ficar legivel */
     }
+}
+
+int main(void) {
+    int vetor[4] = {1, 2, 3, 4};
+    imprime(vetor);
+    return 0;
 }
